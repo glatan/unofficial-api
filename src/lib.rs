a3ccb42da702a57de6d44c7e0fedace5921e99d7
glatan <glatan.edu@gmail.com>
2020-03-12 06:16:22 +0900

    remove old file

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,6 @@
 mod classes;
-mod error;
 mod parse;
-// mod reschedule;
 mod scrape;
 
-// pub use self::reschedule::{Reschedule, Response};
 pub use self::classes::{Canceled, Supplymentaly};
 pub use self::scrape::{Classes, Scrape};
